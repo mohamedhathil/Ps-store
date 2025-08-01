82622ebadb40b7554b47af368df3f236f80ca57e
mohamed_hathil <hathil@dilligentech.com>
2025-08-01 22:19:15 +0530

    [POR-1422] chat option

--- src/app/games-page/games-page.component.ts
+++ src/app/games-page/games-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GAME_STORE_ITEMS } from '../constants';
+import { environment } from 'src/environment';
 
 @Component({
   selector: 'app-games-page',
@@ -8,10 +9,12 @@ import { GAME_STORE_ITEMS } from '../constants';
 })
 export class GamesPageComponent implements OnInit{
   allDatas:any
+  envData:any
   constructor() { }
 
   ngOnInit(): void {
     this.allDatas=GAME_STORE_ITEMS
+    this.envData = `https://wa.me/${environment.client}`;
 
 }
 }
---
+++ src/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  client: '918870572349'
+};
